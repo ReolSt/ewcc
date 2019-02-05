c4d2270cd4ae89659098f3e4e4a6ba388fe65eb2
emacser <roxman98@naver.com>
2019-02-05 19:12:17 +0900

    .

--- mutex_data.cpp
+++
@@ -1,9 +0,0 @@
-#include "mutex_data.h"
-/*
-template <typename TYPE>
-void MutexData<TYPE>::access(std::function<void ()> func) {
-    lock();
-    func();
-    unlock();
-}
-*/
--- mutex_list.cpp
+++
@@ -1,9 +0,0 @@
-#include "mutex_list.h"
-/*
-template <typename TYPE, template<typename ELEM, typename = std::allocator<ELEM>> class LIST>
-void MutexList<TYPE,LIST>::access(std::function<void ()> func) {
-    lock();
-    func();
-    unlock();
-}
-*/
